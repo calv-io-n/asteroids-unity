d17669afb794c70fdba03799f7ff750612ed0650
Calvin Edwards <calvinl.edwards@gmail.com>
2019-05-21 16:13:32 -0700

    Fixed project

---
+++ Assets/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //Thrust
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            //Rotate left
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //Rotate right
+        } 
+    }
+}
