1867c2ca3648d16ec8d97abec5030aa771f0741d
Calvin Edwards <calvinl.edwards@gmail.com>
2019-05-21 19:28:32 -0700

    Fixed .gitignore rules and added levels and foundation for collsion

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,31 +6,77 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource fart;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
 
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();   
+        //Init Unity Components
+        rigidBody = GetComponent<Rigidbody>();
+        fart = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update () {
-        ProcessInput();
+        Thrust();
+        Rotate();
 	}
 
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
     {
-        if (Input.GetKey(KeyCode.Space))
+        switch (collision.gameObject.tag)
         {
-            //Thrust
-            rigidBody.AddRelativeForce(Vector3.up);
+            case "Launch":
+                //Start
+                print("Begin");
+                break;
+            case "Land":
+                //Win
+                break;
+            default:
+                //Loss
+                break;
         }
-        if(Input.GetKey(KeyCode.A))
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //Take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
         {
             //Rotate left
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             //Rotate right
-        } 
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; //Resume Natural physics
+    }
+
+    private void Thrust()
+    {
+        float thrustThisFrame = mainThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //If sound is already playing don't layer sounds
+            if (!fart.isPlaying)
+            {
+                fart.Play();
+            }
+            //Thrust
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+        }
+        else
+        {
+            //Stop sound effect when not depressed
+            fart.Stop();
+        }
     }
 }
